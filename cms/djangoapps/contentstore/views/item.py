ffae532bf316e3897527a3c8ae158fbfe1c482bd
pbischof42 <133028046+pbischof42@users.noreply.github.com>
2024-01-08 14:28:39 -0500

    Update cms/djangoapps/contentstore/views/item.py
    Co-authored-by: Travis McKinney <1715486+tramck@users.noreply.github.com>

--- cms/djangoapps/contentstore/views/item.py
+++ cms/djangoapps/contentstore/views/item.py
@@ -529,7 +529,7 @@ def _update_with_callback(xblock, user, old_metadata=None, old_content=None):
     return modulestore().update_item(xblock, user.id)
 
 @pluggable_override("OVERRIDE_SAVE_XBLOCK")
-def _save_xblock(user, xblock, method, data=None, children_strings=None, metadata=None, nullout=None,  # lint-amnesty, pylint: disable=too-many-statements
+def _save_xblock(user, xblock, data=None, children_strings=None, metadata=None, nullout=None,  # lint-amnesty, pylint: disable=too-many-statements
                  grader_type=None, is_prereq=None, prereq_usage_key=None, prereq_min_score=None,
                  prereq_min_completion=None, publish=None, fields=None):
     """
